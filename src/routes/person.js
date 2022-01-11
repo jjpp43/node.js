b7973f063fcf8196073c2ddd5592b40f6cc88b20
jjpp43 <junnapark@gmail.com>
2022-01-11 07:34:21 +0000

    Differences of routing with parameter or query

--- src/routes/person.js
+++ src/routes/person.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+//QueryString => query property on the request object
+//This allows urls like localhost:3000/person?name=john&age=20
 router.get('/person', (req, res) => {
-	res.send('You have requested person');
+	if(req.query.name) {
+		res.send('You have requested person ${req.query.name}');
+	} else {
+		res.send('You have requested person');
+	}
 });
+
+//Params property on the request object
+//This allows urls like localhost:3000/person/john
+router.get('/person/:name', (req, res) => {
+	res.send('You have requested a person ${req.params.name}');
+})
+
+
 
 module.exports = router;
