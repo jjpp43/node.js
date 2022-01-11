2ea48e47f63320d93ec2807e817546aeaa5f5b75
jjpp43 <junnapark@gmail.com>
2022-01-11 07:05:51 +0000

    api 생성 주석처리

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const express = require('express');
 const app = express();
 const uuidAPIKey = require('uuid-apikey');
 
-
 const server = app.listen(3001, () => {
 	console.log("Start server at localhost:3001");
 });
 //apikey 생성
-console.log(uuidAPIKey.create());
+//console.log(uuidAPIKey.create());
+
+const key = {
+  apiKey: 'P2PSQ41-Z3D48HR-J3KN3BB-E1YD895',
+  uuid: 'b0ad9b90-f8da-4447-90e7-51ad707cd424'
+};
 
 //:을 붙일시에 그 뒤에 오는 것은 변수로 받을 수 있음. 
-app.get('api/user/:type', async (req, res) => {
+app.get('api/user/:apikey/:type', async (req, res) => {
 	let {type} = req.params;
 	
 	//받는 변수에 따라서 동작을 달리할 수 있음
