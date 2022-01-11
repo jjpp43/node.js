f463fa1481cfe2f52d138d40a2ce7275e033d1bb
jjpp43 <junnapark@gmail.com>
2022-01-11 11:46:56 +0000

    Custom handlers for 404 and 500 ERROR

--- src/index.js
+++ src/index.js
@@ -8,10 +8,21 @@ let personRoute = require('./routes/person');
 app.use((req, res, next) => {
     console.log(`${new Date().toString()} => ${req.originalUrl}`);
     next();
-})
+});
 
 app.use(personRoute);
 app.use(express.static('public'));
+
+//Handler for 404 error handler
+app.use((req, res, next) => {
+    res.status(404).send('We think you are lost');
+});
+
+//Handler for 505 internal error
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.info('Server has started on ${PORT}'));
--- src/routes/person.js
+++ src/routes/person.js
@@ -17,6 +17,8 @@ router.get('/person/:name', (req, res) => {
 	res.send('You have requested a person ${req.params.name}');
 })
 
-
+router.get('/error', (req, res) => {
+	throw new Error('This is a forced error');
+});
 
 module.exports = router;
