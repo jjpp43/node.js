51ec106203441d7109711537495cca4282ae0f2a
jjpp43 <junnapark@gmail.com>
2022-01-11 07:23:05 +0000

    router tutorial

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+let personRoute = require('./routes/person');
+
+
+app.use(personRoute);
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.info('Server has started on ${PORT}'));
+
