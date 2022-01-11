8fc213be6327b237ba3ee610f40e128feb5aa7a8
jjpp43 <junnapark@gmail.com>
2022-01-11 12:08:40 +0000

    Custom handlers for 404 and 500 ERROR

---
+++ src/models/customer.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const server = ' ';
+const database = '';
+const user = '';
+const password = '';
+
+mongoose.connect('mongoodb//${user}:${password}@${server}/${database}');
+
+let CustomerSchema = new mongoose.Schema({
+    name: String,
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    }
+});
+
+module.exports = mongoose.model('Customer', CustomerScheme);
